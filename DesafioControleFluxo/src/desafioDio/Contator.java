d8a675636968b1c66c45c6130411227690018db6
Heloísa Fleury <149387177+helofleury@users.noreply.github.com>
2025-04-18 21:17:53 -0300

    Update Contator.java

--- DesafioControleFluxo/src/desafioDio/Contator.java
+++ DesafioControleFluxo/src/desafioDio/Contator.java
@@ -6,9 +6,9 @@ import java.util.Scanner;
 public class Contator {
     public static void main(String[] args) {
         Scanner terminal = new Scanner(System.in);
-        System.out.println("Digite o primeiro parâmetro: ");
+        System.out.print("Digite o primeiro parâmetro: ");
         int parametroUm = terminal.nextInt();
-        System.out.println("Digite o segundo parâmetro: ");
+        System.out.print("Digite o segundo parâmetro: ");
         int parametroDois = terminal.nextInt();
 
         try {
